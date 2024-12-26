1ce205c968dc7a04eaa1598bb0dc520a635e9cfc
Donald <donumeh54@gmail.com>
2024-12-26 01:53:37 +0100

    regexing

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -8,5 +8,8 @@ def filter_datum(
     fields: List[str], redaction: str, message: str, separator: str
 ) -> str:
     """Filter datum returns the log message obfuscated"""
-    return re.sub(r'(' + '|'.join(fields) + r')=[^' + re.escape(separator) + r';]+', r'\1=' + redaction, message)
-
+    return re.sub(
+        r"(" + "|".join(fields) + r")=[^" + re.escape(separator) + r";]+",
+        r"\1=" + redaction,
+        message,
+    )
